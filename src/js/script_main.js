bdc7c836b8a444126714985295fdb4c217522f5e
Ibrahim Ibam <Ibrahim Ibam>
2016-10-12 23:19:30 +0700

    Gameplay hampir jadi walau masih di konsol. sekarang testing buat masukkin backflip image dan coba metode lain buat ngeshuffle image nya, yaitu pake nama file bukan url image nya

--- src/js/script_main.js
+++ src/js/script_main.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 
         //TODO: give condition what state is the button is pressed (stop or start)
 
-        setInterval(countUp, 100);
+        setInterval(countUp, 1);
 
-        $("#startButton").prop("value", "Stop");
+        /*$("#startButton").prop("value", "Stop");*/
 
         function countUp() {
             var timer = $('#timer').find('.stopwatch');
@@ -64,10 +64,88 @@ $(document).ready(function() {
         return array;
     }
 
+    var shuffledArray = shuffleArray(imageUrl);
+
     // memasukkan image ke div tableArea 
 
-    var tableArea = 
+    var tableArea = $("#tableArea");
+    var columnCounter = 1;
+    tableArea.append('<table cellspacing="10">');
 
-    var shuffledArray = shuffleArray(imageUrl);
-    console.log(shuffledArray.toString());
+    var idImage = 0;
+
+    for (var i = 0; i < shuffledArray.length; i++) {
+        var imageUrl = shuffledArray[i];
+
+        if (columnCounter == 1) {
+            tableArea.append("<tr>");
+        }
+
+        tableArea.append('<td width="120" height="120"><img class="clickableImage" src="' + imageUrl + '" width="100" id=image_' + idImage + '></td>')
+
+        columnCounter++;
+
+        if (columnCounter == 5) {
+            tableArea.append("</tr>");
+            columnCounter = 1;
+        }
+
+        idImage++;
+    }
+
+    tableArea.append("</table>");
+
+    var clickedImage = "";
+    var score = 0;
+    var counterClick = 0;
+    var logArea = $("#logGameplay");
+
+    $('.clickableImage').click(function() {
+        var imageID = $(this).prop("id");
+        var imageURL = $(this).prop("src");
+
+        // jika clickedImage empty
+        if (clickedImage != "") {
+
+            // buat kebutuhan debugging aja
+            console.log(imageID);
+            logArea.append(imageID + " opened");
+            logArea.append('<br>');
+
+            var firstImageID = clickedImage;
+            var secondImageID = imageID;
+
+            var firstImage = $("#" + firstImageID);
+            var secondImage = $("#" + secondImageID);
+
+            var firstImageUrl = $("#" + firstImageID).prop("src");
+            var secondImageUrl = $('#' + secondImageID).prop("src");
+
+            // membandingkan apakah kedua image merupakan image yang sama
+            // jika sama maka score bertambah, image tidak bisa diclick lagi (unclickable)
+            // jika beda, maka score tetap, image tetap bisa di click lagi, image flipped, first image dihapus, dan debug clicked image dihapus
+
+            if (firstImageUrl == secondImageUrl) {
+                score += 1;
+                clickedImage = "";
+                if (score == 8) {
+                    alert("you win");
+                }
+                console.log("score " + score);
+                logArea.empty();
+            } else {
+                clickedImage = "";
+                logArea.empty();
+            }
+
+        } else {
+            clickedImage = imageID;
+            console.log(clickedImage);
+            logArea.append(imageID + " opened");
+            logArea.append('<br>');
+        }
+
+    });
+
+
 });
