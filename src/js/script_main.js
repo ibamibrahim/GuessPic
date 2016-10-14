ea9e2579b17b94bc904c5979e78d1807bafe5f79
Ibrahim Ibam <Ibrahim Ibam>
2016-10-14 09:03:01 +0700

    TODO: stop timer, store hasil score, bagusin UI nya kalo sempet

--- src/js/script_main.js
+++ src/js/script_main.js
@@ -52,7 +52,11 @@ $(document).ready(function() {
      * Using Durstenfeld shuffle algorithm.
      * http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
      */
-    var imageUrl = ["src/images/ane.png", "src/images/amru.png", "src/images/ariz.png", "src/images/erlangga.png", "src/images/fadzil.png", "src/images/mail.png", "src/images/mola.png", "src/images/rijal.png", "src/images/ane.png", "src/images/amru.png", "src/images/ariz.png", "src/images/erlangga.png", "src/images/fadzil.png", "src/images/mail.png", "src/images/mola.png", "src/images/rijal.png"]
+
+     /*
+    var imageUrl = ["src/images/ane.png", "src/images/amru.png", "src/images/ariz.png", "src/images/erlangga.png", "src/images/fadzil.png", "src/images/mail.png", "src/images/mola.png", "src/images/rijal.png", "src/images/ane.png", "src/images/amru.png", "src/images/ariz.png", "src/images/erlangga.png", "src/images/fadzil.png", "src/images/mail.png", "src/images/mola.png", "src/images/rijal.png"];*/
+
+    var imageNameArr = ["ane.png", "amru.png", "ariz.png", "erlangga.png", "fadzil.png", "mail.png", "mola.png", "rijal.png", "ane.png", "amru.png", "ariz.png", "erlangga.png", "fadzil.png", "mail.png", "mola.png", "rijal.png"]
 
     function shuffleArray(array) {
         for (var i = array.length - 1; i > 0; i--) {
@@ -64,24 +68,24 @@ $(document).ready(function() {
         return array;
     }
 
-    var shuffledArray = shuffleArray(imageUrl);
+    var shuffledArray = shuffleArray(imageNameArr);
 
     // memasukkan image ke div tableArea 
 
     var tableArea = $("#tableArea");
     var columnCounter = 1;
-    tableArea.append('<table cellspacing="10">');
+    tableArea.append('<table>');
 
     var idImage = 0;
 
     for (var i = 0; i < shuffledArray.length; i++) {
-        var imageUrl = shuffledArray[i];
+        var imageName = shuffledArray[i];
 
         if (columnCounter == 1) {
             tableArea.append("<tr>");
         }
 
-        tableArea.append('<td width="120" height="120"><img class="clickableImage" src="' + imageUrl + '" width="100" id=image_' + idImage + '></td>')
+        tableArea.append('<td width="120" height="120"><img class="clickableImage" src= "src/images/back.png" width="100" id=image_' + idImage + ' alt="' + imageName + '"></td>')
 
         columnCounter++;
 
@@ -100,52 +104,79 @@ $(document).ready(function() {
     var counterClick = 0;
     var logArea = $("#logGameplay");
 
+ /*   var counterasd = 1; 
+    setInterval(function(){
+    	console.log(counterasd + ". clickedImage " + clickedImage);
+    	counterasd++;
+    }, 2000);*/
+
     $('.clickableImage').click(function() {
         var imageID = $(this).prop("id");
-        var imageURL = $(this).prop("src");
+        var imageURL = $(this).prop("alt");
+
+       	$(this).prop("src", "src/images/" + imageURL);
+
 
         // jika clickedImage empty
         if (clickedImage != "") {
 
             // buat kebutuhan debugging aja
-            console.log(imageID);
-            logArea.append(imageID + " opened");
-            logArea.append('<br>');
-
             var firstImageID = clickedImage;
             var secondImageID = imageID;
 
             var firstImage = $("#" + firstImageID);
             var secondImage = $("#" + secondImageID);
 
-            var firstImageUrl = $("#" + firstImageID).prop("src");
-            var secondImageUrl = $('#' + secondImageID).prop("src");
+            var firstImageUrl = $("#" + firstImageID).prop("alt");
+            var secondImageUrl = $('#' + secondImageID).prop("alt");
 
             // membandingkan apakah kedua image merupakan image yang sama
             // jika sama maka score bertambah, image tidak bisa diclick lagi (unclickable)
             // jika beda, maka score tetap, image tetap bisa di click lagi, image flipped, first image dihapus, dan debug clicked image dihapus
 
             if (firstImageUrl == secondImageUrl) {
+
+            	secondImage.removeClass("clickableImage");
+
                 score += 1;
                 clickedImage = "";
+                
                 if (score == 8) {
-                    alert("you win");
+                    alert("Game over!");
                 }
+                
                 console.log("score " + score);
-                logArea.empty();
             } else {
+
                 clickedImage = "";
-                logArea.empty();
+
+                console.log("clickedImage" + clickedImage);
+
+                firstImage.addClass("clickableImage");
+                
+                var interval = setInterval(closeImage, 500);
+
+                function closeImage(){
+                   	firstImage.prop("src", "src/images/back.png");
+                	secondImage.prop("src", "src/images/back.png");
+                	clearInterval(interval);
+                }
             }
 
         } else {
             clickedImage = imageID;
+			var clicked = $("#" + imageID);
+
+			clicked.removeClass("clickableImage");
+
             console.log(clickedImage);
-            logArea.append(imageID + " opened");
-            logArea.append('<br>');
+        
         }
 
     });
 
 
 });
+
+// TODO: stop timer, store hasil score, bagusin UI nya kalo sempet
+// http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
